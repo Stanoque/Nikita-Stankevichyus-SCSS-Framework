07330a5e48ddc215438c200a8c53cf62ec18bb85
Stanoque <stanoquedzhumpsh@gmail.com>
2020-05-04 19:44:31 +0300

    Code commented. FontAwesome added.

--- src/index.js
+++ src/index.js
@@ -2,15 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 
+function Icon(props) {
+  let iconClass;
+
+  if (props.name === 'header'){
+    iconClass = 'fa-angle-double-up';
+  } else if (props.name === 'main') {
+    iconClass = 'fa-align-justify';
+  } else if (props.name === 'aside') {
+    iconClass = 'fa-angle-double-left';
+  } else {
+    iconClass = 'fa-angle-double-down';
+  }
+
+  return (
+    <i className = {'fas ' + iconClass}></i>
+  )
+}
+
 function Default(props) {
   
     return (
       <div className = 'container pd-15'>
-        <h1 className = 'mg-b-10'>{props.name}</h1>
+        <h1 className = 'mg-b-10'>
+          < Icon name = {props.name} />
+          <span className = 'mg-l-5'>{props.name}</span>
+        </h1>
         <p className = 'mg-b-10'>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat
         </p>
-        <a>Дело победителей угодно Богам, но дело проигравших -- Катону.</a>
+        <a>Дело победителей было угодно Богам, но дело побежденных — Катону.</a>
       </div>
     )
   
