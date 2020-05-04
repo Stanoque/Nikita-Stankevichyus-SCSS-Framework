6aa1ff405b456067ca19c6ccda94d5730643d57e
Stanoque <stanoquedzhumpsh@gmail.com>
2020-05-04 16:37:30 +0300

    Margins and paddings are added. Now partial color and size pallets are set in index.scss.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './index.scss';
+
+function Default(props) {
+  
+    return (
+      <div className = 'container pd-15'>
+        <h1 className = 'mg-b-10'>{props.name}</h1>
+        <p className = 'mg-b-10'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat
+        </p>
+        <a>Дело победителей угодно Богам, но дело проигравших -- Катону.</a>
+      </div>
+    )
+  
+}
+
+
+class Framework extends React.Component {
+  render() {
+    return (
+      <div className="body mg-l-100 mg-r-100">
+        <header className="header mg-t-50">
+          <Default name='header' />
+        </header>
+        <main className="main mg-t-50">
+          <Default name='main' />
+        </main>
+        <aside className="aside mg-t-50">
+          <Default name='aside' />
+        </aside>
+        <footer className="footer mg-t-50">
+          <Default name='footer' />
+        </footer>
+      </div>
+    )
+  }
+}
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Framework />,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
